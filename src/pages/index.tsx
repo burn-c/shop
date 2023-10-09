6bfd9f4e6b7586dc8804e8960579fd00a890cda2
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-09 05:27:33 -0300

    style: configure global styles and ssr styles from stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  background: '$rocketseat',
+  borderRadius: 4,
+  border: 0,
+  padding: '4px 8px',
+})
+
 export default function Home() {
-  return <h1>hello</h1>
+  return <Button>hello</Button>
 }
