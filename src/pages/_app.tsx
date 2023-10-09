170073585cbe5ef579d4f787e9d63626c08befd3
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-09 05:11:39 -0300

    style: adds Roboto font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,16 @@
 import type { AppProps } from 'next/app'
 
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
