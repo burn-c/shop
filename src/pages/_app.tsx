a1630259d1a3214b5dcdbbc7a40cd10cab29430d
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-09 20:56:57 -0300

    feat(comp): Adds Content and Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import type { AppProps } from 'next/app'
 
 import { Roboto } from 'next/font/google'
+
+import logoImg from '@assets/logo.svg'
+import { Container, Header } from '@stylesPages/app'
 import { globalStyles } from '../styles/global'
 
 const roboto = Roboto({
   weight: ['400', '700'],
   subsets: ['latin'],
+  variable: '--roboto',
 })
 
 globalStyles()
@@ -13,7 +17,13 @@ globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div className={roboto.className}>
-      <Component {...pageProps} />
+      <Container>
+        <Header>
+          <img src={logoImg.src} alt="" />
+        </Header>
+
+        <Component {...pageProps} />
+      </Container>
     </div>
   )
 }
