20fe316f444a97dfb8142030c40158a009c9a36f
Carlos Oliveira <carlos.burndev@gmail.com>
2023-10-10 02:05:50 -0300

    chore(Home-assets): removed images unused

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,6 @@ import { useKeenSlider } from 'keen-slider/react'
 
 import { HomeContainer, Product } from '@stylesPages/home'
 
-import camiseta1 from '@assets/camisetas/1.png'
-import camiseta2 from '@assets/camisetas/2.png'
-import camiseta3 from '@assets/camisetas/3.png'
-import camiseta4 from '@assets/camisetas/4.png'
-import camiseta5 from '@assets/camisetas/5.png'
-
 import 'keen-slider/keen-slider.min.css'
 import { stripe } from 'src/lib/stripe'
 import Stripe from 'stripe'
